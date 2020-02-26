1dbcf68f79481bad91db44e837b4e64210998c69
maridoh13 <maridoh@gmail.com>
2020-02-26 05:38:18 +0000

    dynamic tweets working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,10 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
+// var moment = require('moment');
+
 
 $(document).ready(() => {
 
-  let data = [
+  const data = [
     {
       "user": {
         "name": "Newton",
@@ -31,33 +33,38 @@ $(document).ready(() => {
     }
   ]
 
-  // function createTweetElement(tweetObj) {
+ 
 
-  //   return tweetObj
-  // }
-
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (let tweet of tweets) {
+      let value = createTweetElement(tweet);
+      $('#tweets-container').append(value);
+    }
+    
   }
+  
 
-  const tweet = `
-  <img id="icon" src="${tweetData.user.avatars}" alt="avatar icon"></img>
-  <p id="nameOfUser">${tweetData.user.name}</p>
-  <p id="tweetUsername">${tweetData.user.handle}</p>
-  <p id="tweetText">${tweetData.content.text}</textarea>
-  <footer id="timeStamp">${tweetData.created_at}</footer>
-  `
-  $('#tweets-container').append(tweet);
+  const createTweetElement = function(tweet) {
+    // let $tweet = $('<article>').addClass('tweet');
+    const $tweet = `
+    <article class="tweetBox">
+    <img id="icon" src="${tweet.user.avatars}" alt="avatar icon"></img>
+    <p id="nameOfUser">${tweet.user.name}</p>
+    <p id="tweetUsername">${tweet.user.handle}</p>
+    <p id="tweetText">${tweet.content.text}</p>
+    <footer id="timeStamp">${moment(tweet['created_at']).fromNow()}</footer>
+    </article>
+    `
+    return $tweet;
+  }
+  
+  renderTweets(data);
 
 
+  
 
 })
